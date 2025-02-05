151c6ad450909a0504a0676095e90a3b71ccac33
Muhammed  Ahad <maaahad@gmail.com>
2025-02-06 00:05:25 +0100

    format

--- src/atoms/Button/__docs__/Button.stories.tsx
+++ src/atoms/Button/__docs__/Button.stories.tsx
@@ -4,35 +4,35 @@ import Example from "./Example";
 import { AranThemeProvider } from "../../../themes";
 
 const meta: Meta<typeof Example> = {
-	title: "Button",
-	component: Example,
-	decorators: [
-		(Story) => (
-			<AranThemeProvider theme="green">
-				<Story />
-			</AranThemeProvider>
-		),
-	],
+  title: "Button",
+  component: Example,
+  decorators: [
+    (Story) => (
+      <AranThemeProvider theme="green">
+        <Story />
+      </AranThemeProvider>
+    ),
+  ],
 };
 
 export default meta;
 type Story = StoryObj<typeof Example>;
 
 export const Primary: Story = {
-	args: {
-		text: "Button",
-		primary: true,
-		disabled: false,
-		size: "small",
-		onClick: () => console.log("Button"),
-	},
+  args: {
+    text: "Button",
+    primary: true,
+    disabled: false,
+    size: "small",
+    onClick: () => console.log("Button"),
+  },
 };
 export const Secondary: Story = {
-	args: {
-		text: "Button",
-		primary: false,
-		disabled: false,
-		size: "small",
-		onClick: () => console.log("Button"),
-	},
+  args: {
+    text: "Button",
+    primary: false,
+    disabled: false,
+    size: "small",
+    onClick: () => console.log("Button"),
+  },
 };
