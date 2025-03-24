fe4dbb7237552212ad011d74498536802d542221
Muhammed  Ahad <maaahad@gmail.com>
2025-03-24 11:59:54 +0100

    removed unused color code

--- src/tokens/color.ts
+++ src/tokens/color.ts
@@ -3,36 +3,6 @@
 
 import type { DefaultTheme } from "styled-components";
 
-// TODO: (maaahad) remove if not used
-// export enum Gray {
-// 	P10 = "#fbfafd",
-// 	P50 = "#ececef",
-// 	P100 = "#dcdcde",
-// 	P200 = "#bfbfc3",
-// 	P300 = "#a4a3a8",
-// 	P400 = "#89888d",
-// 	P500 = "#737278",
-// 	P600 = "#626168",
-// 	P700 = "#535158",
-// 	P800 = "#434248",
-// 	P900 = "#333238",
-// 	P950 = "#1f1e24",
-// }
-//
-// export enum Green {
-// 	P50 = "#ecf4ee",
-// 	P100 = "#c3e6cd",
-// 	P200 = "#91d4a8",
-// 	P300 = "#52b87a",
-// 	P400 = "#2da160",
-// 	P500 = "#108548",
-// 	P600 = "#217645",
-// 	P700 = "#24663b",
-// 	P800 = "#0d532a",
-// 	P900 = "#0a4020",
-// 	P950 = "#072b15",
-// }
-
 // NOTE: colors names are adopted from the Dragon Ball Z approach
 // Ref: https://moon.io/themes
 export type ThemeMode = "light" | "dark";
