331e5601dc1d1f11a672536e101e0b3af3a24cdf
Muhammed  Ahad <maaahad@gmail.com>
2025-03-26 01:21:21 +0100

    forward ref

--- src/organisms/Card/Card.tsx
+++ src/organisms/Card/Card.tsx
@@ -1,4 +1,5 @@
-import type { FC, PropsWithChildren, ReactNode } from "react";
+import type { FC, PropsWithChildren, PropsWithRef, ReactNode } from "react";
+import React from "react";
 import type { ComponentProps } from "../../utils/types";
 import { Body, CardContainer, Slot } from "./styled";
 
@@ -12,28 +13,36 @@ type Props = ComponentProps & {
 	footer?: ReactNode;
 };
 
-export const Card: FC<PropsWithChildren<Props>> = ({
-	className,
-	headerClassName,
-	bodyClasName,
-	footerClassName,
-	header,
-	children,
-	footer,
-}) => {
-	return (
-		<CardContainer className={className}>
-			{header && (
-				<Slot slot="header" className={headerClassName}>
-					{header}
-				</Slot>
-			)}
-			<Body className={bodyClasName}>{children}</Body>
-			{footer && (
-				<Slot slot="footer" className={footerClassName}>
-					{footer}
-				</Slot>
-			)}
-		</CardContainer>
-	);
-};
+export const Card = React.forwardRef<
+	HTMLElement | React.RefCallback<HTMLElement>,
+	PropsWithChildren<Props>
+>(
+	(
+		{
+			className,
+			headerClassName,
+			bodyClasName,
+			footerClassName,
+			header,
+			children,
+			footer,
+		},
+		ref,
+	) => {
+		return (
+			<CardContainer className={className} ref={ref as any}>
+				{header && (
+					<Slot slot="header" className={headerClassName}>
+						{header}
+					</Slot>
+				)}
+				<Body className={bodyClasName}>{children}</Body>
+				{footer && (
+					<Slot slot="footer" className={footerClassName}>
+						{footer}
+					</Slot>
+				)}
+			</CardContainer>
+		);
+	},
+);
