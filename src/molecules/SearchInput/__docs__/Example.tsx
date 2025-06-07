d780b3b91ff16080cc15fa633ce96645ab331761
Muhammed Ahad <maaahad@gmail.com>
2025-06-07 13:11:27 +0200

    Compount component pattern : input field without style

--- src/molecules/SearchInput/__docs__/Example.tsx
+++ src/molecules/SearchInput/__docs__/Example.tsx
@@ -1,9 +1,9 @@
 import React, { useCallback, useState } from "react";
 import type { ComponentProps, FC } from "react";
 import { CloseLineIcon, SearchIcon } from "../../../atoms";
-import { SearchInput } from "../SearchInput";
+import SearchInput from "../SearchInput";
 
-type Props = ComponentProps<typeof SearchInput>;
+type Props = ComponentProps<typeof SearchInput.Root>;
 
 const Data = [
 	{
@@ -24,46 +24,57 @@ const Data = [
 	},
 ];
 
-const Example: FC<Props> = ({ searchSelect, ...restProps }) => {
-	const [searchOn, setSearchOn] = useState(searchSelect?.options[0].value);
-	const [searchQuery, setSearchQuery] = useState<string>("");
-
-	const [searchResults, setSearchResults] =
-		useState<Required<Props>["dropdown"]["options"]>();
-
-	const handleSearchQueryChange = useCallback((query: string) => {
-		setSearchQuery(query);
-		if (!query) setSearchResults(undefined);
-		else
-			setSearchResults(
-				Data.filter((d) =>
-					d.title.toLowerCase().includes(query.toLowerCase()),
-				).map((d, index) => ({
-					label: d.title,
-					value: d.id,
-					disabled: index % 2 === 0,
-					...d,
-				})),
-			);
-	}, []);
+const Example: FC<Props> = (props) => {
+	const [query, setQuery] = useState<string>("");
+	console.log("new query", query);
 
 	return (
-		<SearchInput
-			{...restProps}
-			searchSelect={
-				searchSelect
-					? { ...searchSelect, onChangeSearchOption: setSearchOn }
-					: undefined
-			}
-			value={searchQuery}
-			onChange={handleSearchQueryChange}
-			dropdown={{
-				options: searchResults,
-				loading: false,
-				onSelect: (value, option) => console.log("selected : ", value, option),
-			}}
-		/>
+		<SearchInput.Root value={query} onInputValueChange={setQuery}>
+			<SearchInput.Input placeholder="Search here...." />
+		</SearchInput.Root>
 	);
 };
 
+// const Example: FC<Props> = ({ searchSelect, ...restProps }) => {
+// 	const [searchOn, setSearchOn] = useState(searchSelect?.options[0].value);
+// 	const [searchQuery, setSearchQuery] = useState<string>("");
+//
+// 	const [searchResults, setSearchResults] =
+// 		useState<Required<Props>["dropdown"]["options"]>();
+//
+// 	const handleSearchQueryChange = useCallback((query: string) => {
+// 		setSearchQuery(query);
+// 		if (!query) setSearchResults(undefined);
+// 		else
+// 			setSearchResults(
+// 				Data.filter((d) =>
+// 					d.title.toLowerCase().includes(query.toLowerCase()),
+// 				).map((d, index) => ({
+// 					label: d.title,
+// 					value: d.id,
+// 					disabled: index % 2 === 0,
+// 					...d,
+// 				})),
+// 			);
+// 	}, []);
+//
+// 	return (
+// 		<SearchInput
+// 			{...restProps}
+// 			searchSelect={
+// 				searchSelect
+// 					? { ...searchSelect, onChangeSearchOption: setSearchOn }
+// 					: undefined
+// 			}
+// 			value={searchQuery}
+// 			onChange={handleSearchQueryChange}
+// 			dropdown={{
+// 				options: searchResults,
+// 				loading: false,
+// 				onSelect: (value, option) => console.log("selected : ", value, option),
+// 			}}
+// 		/>
+// 	);
+// };
+//
 export default Example;
