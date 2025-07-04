afcdc8066856316fa830e6f214fef28a5d9c1e90
Muhammed Ahad <maaahad@gmail.com>
2025-07-05 00:50:08 +0200

    ButtonStyled init

--- src/atoms/Button/__docs__/Example.tsx
+++ src/atoms/Button/__docs__/Example.tsx
@@ -1,37 +1,37 @@
 import React from "react";
-import type { FC, ComponentProps } from "react";
+import type { ComponentProps, FC } from "react";
 import { Button, type Props } from "../Button";
 
 const Example: FC<
 	Omit<ComponentProps<typeof Button>, "onClick" | "children">
 > = ({
 	disabled = false,
-	// onClick = () => { },
 	size = "md",
 	variant = "filled",
 	loading,
 	loadingText,
 }) => {
-		return (
-			<div
-				style={{
-					display: "flex",
-					justifyContent: "center",
-					alignItems: "center",
-					height: "100%",
-				}}
+	return (
+		<div
+			style={{
+				display: "flex",
+				justifyContent: "center",
+				alignItems: "center",
+				height: "100%",
+			}}
+		>
+			<Button
+				disabled={disabled}
+				size={size}
+				variant={variant}
+				loading={loading}
+				loadingText={loadingText}
+				onClick={(event) => console.log(event)}
 			>
-				<Button
-					disabled={disabled}
-					size={size}
-					variant={variant}
-					loading={loading}
-					loadingText={loadingText}
-				>
-					Button
-				</Button>
-			</div>
-		);
-	};
+				Button
+			</Button>
+		</div>
+	);
+};
 
 export default Example;
