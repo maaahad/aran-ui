c8a013d89e1344d13bc9aae170c5e8a91405b033
Muhammed Ahad <maaahad@gmail.com>
2025-07-12 13:47:56 +0200

    semantic colors added

--- src/styled.d.ts
+++ src/styled.d.ts
@@ -14,7 +14,7 @@ declare module "styled-components" {
 	 * It will increase maintainability
 	 */
 
-	export type ThemeColors = {
+	export type Colors = {
 		primary: {
 			white: string;
 			black: string;
@@ -133,85 +133,85 @@ declare module "styled-components" {
 		};
 	};
 
-	export type ThemeSemanticColors = {
-		text: {
-			/**
-			 * Default color for test (e.g. body text)
-			 * Should have high contrast with default background color
-			 */
-			primary: string;
-
-			/**
-			 * Used for secondary text. e.g., subtitles, meta text, labels
-			 * Slightly lower contrast than primary color
-			 */
-			secondary: string;
-
-			/**
-			 * For inactive, placeholder or hint text
-			 * Typically lighter or dimmed gray
-			 */
-			muted: string;
-
-			/**
-			 * Text used for dark/inverted background
-			 */
-			inverted: string;
-
-			/**
-			 * For link, interactive text for ex. buttons, CTAs
-			 * Should have hight visual prominnence
-			 */
-			link: string;
-
-			/**
-			 * Text used for error messages or destructive states
-			 */
-			danger: string;
-
-			/**
-			 * Text used for communicates successfule actions or states
-			 */
-			success: string;
-		};
-		background: {
-			/**
-			 * Main background colors. Used for full-page, default containers
-			 */
-			default: string;
-
-			/**
-			 * used for raised surfaces. for ex. cards, modals, or app bars
-			 */
-			elevated: string;
-
-			/**
-			 * For inputs, muted container, toggles etc
-			 */
-			subtle: string;
-
-			/**
-			 * Background color for darkmode/inverted theme
-			 */
-			inverted: string;
-		};
-		border: {
-			/**
-			 * standard border color. ex. contaienrs, input
-			 */
-			default: string;
-
-			/**
-			 * lighter borders for less emphasis. For ex. card edges, dividers
-			 */
-			muted: string;
-
-			/**
-			 * Strong borders for emphasis. For ex. Focused states, alerts
-			 */
-			strong: string;
-		};
-	};
+	// export type ThemeSemanticColors = {
+	//   text: {
+	//     /**
+	//      * Default color for test (e.g. body text)
+	//      * Should have high contrast with default background color
+	//      */
+	//     primary: string;
+	//
+	//     /**
+	//      * Used for secondary text. e.g., subtitles, meta text, labels
+	//      * Slightly lower contrast than primary color
+	//      */
+	//     secondary: string;
+	//
+	//     /**
+	//      * For inactive, placeholder or hint text
+	//      * Typically lighter or dimmed gray
+	//      */
+	//     muted: string;
+	//
+	//     /**
+	//      * Text used for dark/inverted background
+	//      */
+	//     inverted: string;
+	//
+	//     /**
+	//      * For link, interactive text for ex. buttons, CTAs
+	//      * Should have hight visual prominnence
+	//      */
+	//     link: string;
+	//
+	//     /**
+	//      * Text used for error messages or destructive states
+	//      */
+	//     danger: string;
+	//
+	//     /**
+	//      * Text used for communicates successfule actions or states
+	//      */
+	//     success: string;
+	//   };
+	//   background: {
+	//     /**
+	//      * Main background colors. Used for full-page, default containers
+	//      */
+	//     default: string;
+	//
+	//     /**
+	//      * used for raised surfaces. for ex. cards, modals, or app bars
+	//      */
+	//     elevated: string;
+	//
+	//     /**
+	//      * For inputs, muted container, toggles etc
+	//      */
+	//     subtle: string;
+	//
+	//     /**
+	//      * Background color for darkmode/inverted theme
+	//      */
+	//     inverted: string;
+	//   };
+	//   border: {
+	//     /**
+	//      * standard border color. ex. contaienrs, input
+	//      */
+	//     default: string;
+	//
+	//     /**
+	//      * lighter borders for less emphasis. For ex. card edges, dividers
+	//      */
+	//     muted: string;
+	//
+	//     /**
+	//      * Strong borders for emphasis. For ex. Focused states, alerts
+	//      */
+	//     strong: string;
+	//   };
+	// };
 
 	export type Typography = {
 		fonts: {
@@ -289,7 +289,7 @@ declare module "styled-components" {
 	export interface DefaultTheme {
 		colors: {
 			raw: ThemeColors;
-			semantic: ThemeSemanticColors;
+			semantic: SemanticColors;
 		};
 		typography: Typography;
 		space: Space;
